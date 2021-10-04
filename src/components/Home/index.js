2589952f11599e8a68ee5b1ebb821fa76a2b255a
Diego Ragone <ragonediego87@gmail.com>
2021-10-04 16:56:27 +0200

    2div

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from '../Card';
+
+class Home extends React.Component {
+    constructor () {
+        super ();
+
+        this.state = {
+            articles = []
+        };
+    };
+
+    render () {
+        const cards = this.state.articles.map(card => <Card key={card.id} article = { article }/>);
+
+        return (
+            <div>
+                { cards }
+                <div></div>
+                <div></div>
+            </div>
+        );
+    };
+};
